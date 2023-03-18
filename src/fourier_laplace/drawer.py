03b4b3c5b22786c2baccf27ec3a21a602f612434
andreshyer <hyerap@vcu.edu>
2023-03-18 01:28:28 -0400

    drop all loops

--- src/fourier_laplace/drawer.py
+++ src/fourier_laplace/drawer.py
@@ -1,6 +1,5 @@
 from pathlib import Path
-from math import sqrt, cos, sin, pi
-from numpy import loadtxt, searchsorted, where, array
+from numpy import loadtxt, searchsorted, where, array, sqrt, cos, sin, pi
 
 # Load data files
 a_data = loadtxt(Path(__file__).parent / 'tables/a_data.csv', delimiter=',')
@@ -8,30 +7,16 @@ b_data = loadtxt(Path(__file__).parent / 'tables/b_data.csv', delimiter=',')
 P_data = loadtxt(Path(__file__).parent / 'tables/P_data.csv', delimiter=',')
 max_x_data = loadtxt(Path(__file__).parent / 'tables/max_x_data.csv', delimiter=',')
 
+def estimate(z_values: array, P: float, a_constants: array, b_constants: array) -> array:
+    num_constants = a_constants.size
+    n_values = array(range(1, num_constants))
+    z_values_2d = z_values[:, None]
 
-def estimate(z_values: array, P: float, a_constants: list, b_constants: list):
-    num_constants = len(a_constants)
-    results = []
+    cos_term = cos(2 * pi * n_values * z_values_2d / P)
+    sin_term = sin(2 * pi * n_values * z_values_2d / P)
 
-    for i in range(len(z_values)):
-        z = z_values[i]
-
-        if z < 0.1:
-            results.append(sqrt(2 * z - z ** 2))
-
-        else:
-            x_calc = 0.0
-            for n in range(num_constants):
-                a_n = a_constants[n]
-                b_n = b_constants[n]
-                if n == 0:
-                    x_calc += a_n
-                else:
-                    x_calc += a_n * cos(2 * pi * n * z / P)
-                    x_calc += b_n * sin(2 * pi * n * z / P)
-            results.append(x_calc)
-
-    return results
+    x_calc = a_constants[0] + (a_constants[1:] * cos_term + b_constants[1:] * sin_term).sum(axis=1)
+    return where(z_values < 0.1, sqrt(2 * z_values - z_values ** 2), x_calc)
 
 
 class FourierProfile:
@@ -44,9 +29,8 @@ class FourierProfile:
         
         self.bond_number: int = bond_number
 
-        # Grab variables
-        self.a_constants = list(self._interp_rows(a_data, bond_number))
-        self.b_constants = list(self._interp_rows(b_data, bond_number))
+        self.a_constants = array(self._interp_rows(a_data, bond_number))
+        self.b_constants = array(self._interp_rows(b_data, bond_number))
         self.P = self._interp_rows(P_data, bond_number)[0]
         self.max_x = self._interp_rows(max_x_data, bond_number)[0]
 
@@ -60,13 +44,12 @@ class FourierProfile:
         return lower_row + (bond_number - Bo[idx_lower]) * (upper_row - lower_row) / (Bo[idx_upper] - Bo[idx_lower])
 
     def estimate(self, z: array) -> array:
-
         if len(where(z < 0)[0]) > 0:
             raise ValueError("Value of z less than 0 detected")
         if len(where(z > 5)[0]) > 0:
             raise ValueError("Value of z greater than 5 detected")
 
-        return array(estimate(z, self.P, self.a_constants, self.b_constants))
+        return estimate(z, self.P, self.a_constants, self.b_constants)
     
     def get_max_x(self) -> float:
         return self.max_x
